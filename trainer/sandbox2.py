0afb2bd240c6e4a7a3e2bcc7033263a9033944c9
kudkudak <staszek.jastrzebski@gmail.com>
2014-04-24 10:58:53 +0200

    Added negative precision

--- trainer/sandbox2.py
+++ trainer/sandbox2.py
@@ -24,6 +24,20 @@ import cPickle
 cPickle.dump(pca, open("trained_pca.pkl","w"))
 """
 
+def _tp_tn_fp_fn(y_true, y_pred):
+    tp, tn, fp, fn = 0., 0., 0., 0.
+    for y_t, y_p in zip(y_true, y_pred):
+        if y_t == y_p and y_t == 1:
+            tp += 1
+        if y_t != y_p and y_t == 1:
+            fn += 1
+        if y_t != y_p and y_t == 0:
+            fp += 1
+        if y_t == y_p and y_t == 0:
+            tn += 1
+    return tp, tn, fp, fn
+
+
 #print "PCA ratios: ", pca.explained_variance_ratio_
 
 clf = RandomForestClassifier(n_estimators=24, max_features=250,
@@ -32,9 +46,9 @@ clf = RandomForestClassifier(n_estimators=24, max_features=250,
 clf.fit(X_tr, Y_tr)
 
 print clf.feature_importances_
+tp, tn, fp, fn = _tp_tn_fp_fn(Y_st, clf.predict(X_tst))
 
-print clf.score(X_tst, Y_st)
-
+print "Accuracy ", (tp+tn)/(tp+tn+fp+fn), "Negative precision ", tn/(tn+fn)
 
 cPickle.dump(clf, open("rndforest.pkl","w"))
 
