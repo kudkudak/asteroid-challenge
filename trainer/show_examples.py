a3e52b5be6224b2b01a13e351900d67c47619915
kudkudak <staszek.jastrzebski@gmail.com>
2014-04-26 13:37:09 +0200

    Improve show_examples

--- trainer/show_examples.py
+++ trainer/show_examples.py
@@ -1,13 +1,16 @@
 from generate_aug_data import *
 import matplotlib.pylab as plt   
 from im_operators import * 
+from visualize import *
 for i in xrange(1,10000):
     ex, label = load_img_det(i)
     if label[-1] == '0':
         print label
         print ex.shape
-        plt.title(str(label[-1]))
-        plt.imshow(im_crop(ex.reshape(4,64,64)[0],4.0), cmap='hot')
-        plt.show() 
+        im = [im_crop(j, 3.0) for j in ex.reshape(4,64,64)]
+        show_4_ex(im, label, title=label[-1])
+        #plt.title(str(label[-1]))
+        #plt.imshow(im_crop(ex.reshape(4,64,64)[0],4.0), cmap='hot')
+        #plt.show() 
 
 
